38373cb0ad16394b717acde8c2f181bb1df2391d
shraf <eldeenshraf@gmail.com>
2022-03-19 23:47:03 +0200

    Update csvUtil.py

--- csvUtil.py
+++ csvUtil.py
@@ -1,34 +1,33 @@
 from queue import Queue
 from threading import *
-import pandas as pd 
+from CsvReaderAsync import CsvReaderAsync
+from CsvFilter import CsvFilter
+import pandas as pd
 
-class CsvReaderAsync(Thread):
-    def __init__(self, q, filename, SENTINEL):
-        super().__init__()
-        self.q = q
-        self.filename=filename
-        self.SENTINEL = SENTINEL
-    def read(self):
-        chunksize=10**3
-        with pd.read_csv(self.filename, on_bad_lines='skip', chunksize=chunksize, encoding="ISO-8859-1") as reader:
-            for chunk in reader:
-                self.q.put(chunk)
-        self.q.put(self.SENTINEL)
-        
+q =  Queue()
+def onReadChunk(chunk):
+    q.put(chunk)
 
-    def run(self):
-        self.read()
+def onFinishReading():
+    print("done")
+
+def onFilterMatch(record):
+    print(record[1].iloc[0], "unhealthy")
+def onFilterFailure(record):
+    print (record, "healthy")
+
 
 def main():
     SENTINEL = object()
-    q =  Queue()
-    csv_reader =  CsvReaderAsync(q,"./ofile.csv", SENTINEL)
+    badwords = pd.read_csv("./badWords.csv",header=None)
+    csv_reader =  CsvReaderAsync(onReadChunk,onFinishReading,"./Hussien1.csv", SENTINEL)
     csv_reader.start()
+    csv_filter = CsvFilter(onFilterMatch,onFilterFailure, badwords)
     while(True):
-        if(q.not_empty):
-            data = q.get()
-            if(data is SENTINEL):
-                break
+        data = q.get()
+        csv_filter.FilterWords(data)
+        if(data is SENTINEL):
+            break
     print("done")
 if __name__ == '__main__':
     main()
