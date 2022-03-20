d72b492d652be24e80d66b6b3ccde5fb55337bb6
shraf <eldeenshraf@gmail.com>
2022-03-20 03:46:32 +0200

    Update CsvReaderAsync.py

--- CsvReaderAsync.py
+++ CsvReaderAsync.py
@@ -2,12 +2,12 @@ from threading import *
 import pandas as pd 
 
 class CsvReaderAsync(Thread):
-    def __init__(self, onReadChunk, onFinishReading, filename, SENTINEL):
+    def __init__(self, onReadChunk, onFinishReading, filename):
         super().__init__()
         self.onReadChunk = onReadChunk
         self.onFinishReading=onFinishReading
         self.filename=filename
-        self.SENTINEL = SENTINEL
+        
     def read(self):
         chunksize=10**3
         with pd.read_csv(self.filename,na_filter=False, on_bad_lines='skip', chunksize=chunksize, encoding="ISO-8859-1") as reader:
