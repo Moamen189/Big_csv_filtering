23af95d4e888262a19a12cd154079c338b061b44
shraf <eldeenshraf@gmail.com>
2022-03-20 03:46:59 +0200

    Update CsvFilter.py

--- CsvFilter.py
+++ CsvFilter.py
@@ -9,20 +9,19 @@ class CsvFilter:
         badWordsList= self.word_list.values.tolist()
         self.badWordsRegex = re.compile('|'.join(re.escape(x[0]) for x in badWordsList))
         #print(self.badWordsRegex)
+        
     def FilterWords(self,chunk):
         if(type(chunk)==(object)):
             return 0
 
-        for record in chunk.iterrows():
+        for index,record in chunk.iterrows():
             recordunhealthy=False
-            #print(record[1])
-            #print(record[1].iloc[2])
-            if re.search(self.badWordsRegex , record[1].iloc[0]) != None or re.match(self.badWordsRegex , record[1].iloc[2]) != None or re.match(self.badWordsRegex , record[1].iloc[4]) != None:
+            if re.search(self.badWordsRegex , record[0]) != None or re.match(self.badWordsRegex , record[2]) != None or re.match(self.badWordsRegex , record[4]) != None:
                 recordunhealthy=True
-            else:
-                self.onFailure(record)
+
                 
             if recordunhealthy:
                 self.onMatch(record)
-
+            else:
+                self.onFailure(record)
            
